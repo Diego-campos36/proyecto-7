7a3c206d9dcca69f5ade465bc2ff97b0ffa177f5
Diego-campos36 <diegoemmanuelaleman24@cbtis52.edu.mx>
2026-05-07 14:28:09 -0600

    HUEVA

--- bajar.js
+++ bajar.js
@@ -68,3 +68,36 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
 });
+
+
+document
+.getElementById("loginForm")
+.addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    const usuario = document.getElementById("usuario").value;
+    const password = document.getElementById("password").value;
+
+    const mensaje = document.getElementById("mensaje");
+
+    if(usuario === "admin" && password === "1234"){
+
+        mensaje.textContent = "Login correcto";
+        mensaje.style.color = "limegreen";
+
+        setTimeout(() => {
+            window.location.href = "index.html";
+        }, 1000);
+
+    } 
+    
+    else {
+
+        mensaje.textContent = "Usuario o contraseña incorrectos";
+        mensaje.style.color = "red";
+
+    }
+
+});
+
